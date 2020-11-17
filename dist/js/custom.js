7436caa0a098f1deb4121f546acfb8d12730915d
Wilson Delgado <wilson.dlgd0@gmail.com>
2020-11-17 11:08:25 -0600

    Added animation & fix size of text on navbar

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,3 +1,13 @@
 $(function() {
     const $gallery = $('.gallery a').simpleLightbox();
 })
+
+// Animate Smooth Scroll
+$('#view-about').on('click', function() {
+    const images = $('#about').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 
+    900);
+});
